e96ae78f6d2682f67f1ab215d232ff8f1c916bb7
Mátyás <58818446+matyasosvath@users.noreply.github.com>
2024-10-23 18:16:03 +0200

    Update generation.py

--- backend-service/src/service/generation.py
+++ backend-service/src/service/generation.py
@@ -7,10 +7,6 @@ MAX_NEW_TOKENS = 1024 # seq len for gpt2-small
 
 def generate(text: str, context: str):
 
-    """
-    curl 127.0.0.1:80/generate -X POST -d '{"inputs":"What is deep learning?","parameters":{"max_new_tokens":100, "temperature": 1.3}}' -H 'Content-Type: application/json'
-    """
-
     # crop current text + context to max sequence length
     input = "Question:\n" + text + "\nContext:\n" + context
     input = input if len(input) <= MAX_NEW_TOKENS else input[:MAX_NEW_TOKENS]
